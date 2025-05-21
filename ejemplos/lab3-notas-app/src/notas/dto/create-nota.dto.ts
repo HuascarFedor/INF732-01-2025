59dd2e608d795670f7f5190c6f1a013927c8bdd3
HuascarFedor <huascar.fedor@gmail.com>
2025-05-20 21:48:40 -0400

    added laboratorio 7

--- ejemplos/lab3-notas-app/src/notas/dto/create-nota.dto.ts
+++ ejemplos/lab3-notas-app/src/notas/dto/create-nota.dto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateNotaDto {
   @IsString()
   @IsNotEmpty({ message: 'The title is required' })
+  @ApiProperty({ example: 'Salir de paseo', description: 'Titulo de la nota' })
   title: string;
 
   @IsString()
   @IsNotEmpty({ message: 'The content is required' })
+  @ApiProperty({
+    example: 'Preparar las cosas para salir de paseo',
+    description: 'Descripcion de la nota',
+  })
   content: string;
 }
--- ejemplos/lab3-notas-app/src/notas/notas.controller.ts
+++ ejemplos/lab3-notas-app/src/notas/notas.controller.ts
@@ -11,17 +11,33 @@ import { NotasService } from './notas.service';
 import { Nota } from './nota.entity';
 import { CreateNotaDto } from './dto/create-nota.dto';
 import { UpdateNotaDto } from './dto/update-nota.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Notas')
 @Controller('notas')
 export class NotasController {
   constructor(private readonly notasService: NotasService) {}
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Nota creada correctamente' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos' })
+  @ApiOperation({
+    summary: 'Crear una nota',
+    description: 'Este endpoint crea una nueva nota en el sistema.',
+  })
+  @ApiBody({ type: CreateNotaDto })
   async create(@Body() createNotaDto: CreateNotaDto): Promise<Nota> {
     return this.notasService.create(createNotaDto);
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Nota ha sido encontrada' })
+  @ApiResponse({ status: 404, description: 'La nota no existe en el sistema' })
+  @ApiOperation({
+    summary: 'Encontrar una nota',
+    description: 'Este endpoint permite encontrar una nota en el sistema.',
+  })
+  @ApiParam({ name: 'id', description: 'ID de la nota' })
   async findOne(@Param('id') id: string): Promise<Nota> {
     return this.notasService.findOne(+id);
   }
